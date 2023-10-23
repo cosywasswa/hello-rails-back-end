39a63378aa81b534e506da193707bb3a38c80ecc
Cosmas Wasswa <cosywas@gmail.com>
2023-10-23 13:52:31 +0300

    Add route to greetings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'api/v1/greetings#index'
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/index'
+     
+    end
+  end
 end
